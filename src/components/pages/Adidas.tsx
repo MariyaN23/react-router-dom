aae3eb9419118fe3fad946209183b244b554d691
MariyaN23 <sellaite505@gmail.com>
2024-03-24 12:27:07 +0300

    react-router-dom base project

--- src/components/pages/Adidas.tsx
+++ src/components/pages/Adidas.tsx
@@ -3,15 +3,18 @@ import adidasModel1 from './../../assets/AdiFOM_TRXN_Shoes_Black_IG7453_01_stand
 import adidasModel2 from './../../assets/Superstar_XLG_Shoes_Black_IG9777_01_standard.webp'
 import adidasModel3
     from './../../assets/PostMove_Mid_Cloudfoam_Super_Lifestyle_Basketball_Mid_Classic_Shoes_Black_GY7163_01_standard.webp'
+import {Link} from 'react-router-dom';
 
 export type AdidasItem = {
-    model: string;
-    collection: string;
-    price: string;
-    picture: string;
+    model: string
+    collection: string
+    price: string
+    picture: string
+    id: number
 }
 export const adidasArr: AdidasItem[] = [
     {
+        id: 1,
         model: 'ADIDAS ADIFOM TRXN',
         collection: 'new collection1',
         price: '100200$',
@@ -19,12 +22,14 @@ export const adidasArr: AdidasItem[] = [
 
     },
     {
+        id: 2,
         model: 'ADIDAS ADIFOM SUPER',
         collection: 'new collection22',
         price: '200300$',
         picture: adidasModel2
     },
     {
+        id: 3,
         model: 'ADIDAS SUPER SUPERSKI',
         collection: 'new collection333',
         price: '300400$',
@@ -32,10 +37,17 @@ export const adidasArr: AdidasItem[] = [
     }
 ]
 
-export const PageOne = () => {
+export const Adidas = () => {
     return (
         <div>
-            <h2 style={{textAlign: 'center'}}> ADIDAS</h2>
+            <h2 style={{textAlign: 'center'}}>ADIDAS</h2>
+            <div>{adidasArr.map((adidas, index) => (
+                <Link key={index} to={`/adidas/${adidas.id}`}>
+                    <img src={adidas.picture}
+                    alt={adidas.model}
+                    style={{width: '200px', height: 'auto', marginRight: '10px'}}/>
+                </Link>
+            ))}</div>
             <p>
                 What is Lorem Ipsum?
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
